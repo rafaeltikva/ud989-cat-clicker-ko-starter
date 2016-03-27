5ead916f6a4d07cee2b92d37e82501139878b78e
Rafael Tikva <rafaeltikva@Rafaels-MacBook-Pro.local>
2016-03-27 05:47:05 +0300

    Replace new cat nickname jQuery value with binding the catNickNameInput observer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ViewModel = function() {
+var Cat = function() {
     var self = this;
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
@@ -6,34 +6,43 @@ var ViewModel = function() {
     this.imgAttribution = ko.observable('https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg');
     this.catNicknames = ko.observableArray(['Ronaldo', 'Messi', 'Suarez', 'Drinkwater', 'Vardy']);
 
-    this.addCatNickname = function() {
-        var nickname = $('#cat-nickname').val();
-        this.catNicknames.push(nickname);
-    };
-    this.removeCatNickname = function() {
-        var nickname = $('#cat-nickname').val();
-        var foundCatIndex = this.catNicknames().indexOf(nickname);
-        if (foundCatIndex >= 0) {
-            this.catNicknames.splice(foundCatIndex, 1);
-        }
+};
 
-    };
+var ViewModel = function() {
+
+    this.currentCat = ko.observable(new Cat());
 
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     };
 
     this.catLevel = ko.computed(function() {
-        console.log("the clickCount: " + this.clickCount());
-        if (this.clickCount() >= 10 && this.clickCount() < 20) {
+        if (this.currentCat().clickCount() >= 10 && this.currentCat().clickCount() < 20) {
             return "Infant";
         }
-        if (this.clickCount() >= 20) {
+        if (this.currentCat().clickCount() >= 20) {
             return "Teen";
         }
         return "Newborn";
 
     }, this);
+
+    this.catNickNameInput = ko.observable('');
+
+    this.addCatNickname = function() {
+        // var nickname = $('#cat-nickname').val(); - replaced with 'value' bind
+        var nickname = this.catNickNameInput();
+        this.currentCat().catNicknames.push(nickname);
+    };
+    this.removeCatNickname = function() {
+        // var nickname = $('#cat-nickname').val();
+        var nickname = this.catNickNameInput();
+        var foundCatIndex = this.currentCat().catNicknames().indexOf(nickname);
+        if (foundCatIndex >= 0) {
+            this.currentCat().catNicknames.splice(foundCatIndex, 1);
+        }
+
+    };
 
 
 };
