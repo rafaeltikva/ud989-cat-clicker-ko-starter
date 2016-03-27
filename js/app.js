4862153d1df1b5e9f2c8bdebc7162e3d687da60e
Rafael Tikva <rafaeltikva@Rafaels-MacBook-Pro.local>
2016-03-27 05:03:46 +0300

    Add cat nicknames and option to add/remove cat nicknames
    * List of 5 cat nicknames built in
    * Bind each cat nickname to a ul element with a foreach binding
    * Create a new input field for adding/removing cats from the observableArray.
    	The Add & Remove buttons are binded to addCatNickname & removeCatNickname respectively

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 var ViewModel = function() {
+    var self = this;
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg');
+    this.catNicknames = ko.observableArray(['Ronaldo', 'Messi', 'Suarez', 'Drinkwater', 'Vardy']);
+
+    this.addCatNickname = function() {
+        var nickname = $('#cat-nickname').val();
+        this.catNicknames.push(nickname);
+    };
+    this.removeCatNickname = function() {
+        var nickname = $('#cat-nickname').val();
+        var foundCatIndex = this.catNicknames().indexOf(nickname);
+        if (foundCatIndex >= 0) {
+            this.catNicknames.splice(foundCatIndex, 1);
+        }
+
+    };
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
@@ -19,6 +34,8 @@ var ViewModel = function() {
         return "Newborn";
 
     }, this);
+
+
 };
 
 ko.applyBindings(new ViewModel());
